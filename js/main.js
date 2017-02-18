79bbb39695eed9ee448c3376502324eb24b309ec
Nadia Pillard <ua9ond@yandex.ru>
2017-02-18 15:08:46 +0700

    services styles added, service-item focus by js added

--- js/main.js
+++ js/main.js
@@ -7,4 +7,14 @@ $(document).ready(function() {
   answer.slideToggle(300);
   currentIteml.toggleClass('faq__item').toggleClass('faq__item faq__item--active');
   });
+
+  $('.services__btn').on('mousedown', function(event){
+    var currentItem = $(event.target).closest('.services__item');
+    currentItem.addClass('services__item--focus');
+  });
+  $('.services__btn').on('mouseup', function(event){
+    var currentItem = $(event.target).closest('.services__item');
+    currentItem.removeClass('services__item--focus');
+  });
+  //add class active for services__item when add burron is clicked
 });
