0cb97a1a8da92e3bec3475eec02dea12d223a923
Nadia Pillard <ua9ond@yandex.ru>
2017-02-18 21:22:34 +0600

    info section done

--- js/main.js
+++ js/main.js
@@ -2,19 +2,30 @@ $(document).ready(function() {
 
   //show/hide questions
   $('.faq__question').on('click', function(event){
-  var currentIteml = $(event.target).closest('.faq__item');
-  var answer = currentIteml.find('.faq__answer');
+  var currentItem = $(event.target).closest('.faq__item');
+  var answer = currentItem.find('.faq__answer');
   answer.slideToggle(300);
-  currentIteml.toggleClass('faq__item').toggleClass('faq__item faq__item--active');
+  currentItem.toggleClass('faq__item').toggleClass('faq__item faq__item--active');
   });
+  
+  //show/hide additional info in section info
+  $('.info__addit-btn').on('click', function(event){
+    $('.info__addit').slideToggle(300);
+  })
+  
+  $('.info__btn--more').on('click', function(event){
+    $('.info__addit').slideToggle(300);
+  })
 
+  //add class active for services__item when add burron is clicked
   $('.services__btn').on('mousedown', function(event){
     var currentItem = $(event.target).closest('.services__item');
     currentItem.addClass('services__item--focus');
   });
+  
   $('.services__btn').on('mouseup', function(event){
     var currentItem = $(event.target).closest('.services__item');
     currentItem.removeClass('services__item--focus');
   });
-  //add class active for services__item when add burron is clicked
+  
 });
