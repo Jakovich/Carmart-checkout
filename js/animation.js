0c851fe56720cb637becefe52d612173eab5dc14
Nadia Pillard <ua9ond@yandex.ru>
2017-03-31 01:05:15 +0700

    animation done

--- js/animation.js
+++ js/animation.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 
     function addIcon(context, target) {
         var currentIcon = $(context).closest('.services__item').find('.services__icon img');
-        var target = $(target);
         var iconClone = currentIcon.clone().appendTo("body");
         iconClone.css({
             top: $(currentIcon).offset().top,
@@ -21,7 +20,6 @@ $(document).ready(function() {
                 width: '30px',
                 height: '30px'
             }, 500,
-
             function() {
                 $(this).remove();
 
@@ -29,15 +27,12 @@ $(document).ready(function() {
     }
 
 
-
-    function blink(elem,color) {
-        var priceAuto = $(elem);
-        priceAuto.css({
+    function blink(elem, color) {
+        elem.css({
             color: '#00ab8a'
         })
-
         setTimeout(function() {
-            priceAuto.css({
+            elem.css({
                 color: color
             })
         }, 200)
@@ -45,24 +40,24 @@ $(document).ready(function() {
     }
 
 
-
+    //example of animations
     $('main .services__items .row:nth-child(1) .services-btn').on('click', function(event) {
-        var target = '.info__price';
+        var target = $('.info__price');
         if ($(this).hasClass('services-btn--select')) {
             addIcon(this, target);
             setTimeout(function() {
-                $(target).html('1 200 500 Р');
+                target.html('1 200 500 Р');
                 blink(target, '#ffffff');
             }, 550)
         }
     });
 
     $('main .services__items .row:nth-child(2) .services-btn').on('click', function(event) {
-        var target = '.info__sum span';
+        var target = $('.info__sum span');
         if ($(this).hasClass('services-btn--select')) {
             addIcon(this, target);
             setTimeout(function() {
-                $(target).html('1 500 000 Р');
+                target.html('1 500 000 Р');
                 blink(target, 'rgba(255,255,255,0.7)');
             }, 550)
         }
